2f7ef620a01f474e6fa920c8d1a7c4e297c9786f
Zach Underwood <zunder1990@gmail.com>
2017-02-08 22:31:19 -0500

    update

--- SCALE15x/wireless/installzabbix.sh
+++ SCALE15x/wireless/installzabbix.sh
@@ -3,6 +3,7 @@ cd ./configap
 for i in $(ls -d */)
 do 
 #echo ${i%%/} 
-ssh root@"${i%%/}" "opkg update && opkg install zabbix-agentd zabbix-extra-mac80211 zabbix-extra-network zabbix-extra-wifi "
-
+ssh -i ../id_rsa root@"${i%%/}" "opkg update && opkg install zabbix-agentd zabbix-extra-mac80211 zabbix-extra-network zabbix-extra-wifi "
+scp -i ../id_rsa ../zabbix_agentd.conf root@"${i%%/}":/etc/zabbix_agentd.conf
+ssh -i ../id_rsa root@"${i%%/}" "/etc/init.d/zabbix_agentd restart"
 done
