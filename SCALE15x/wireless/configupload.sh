2f0790170125fff5fee4aef22c7cddfd582826ad
kylerisse <kylerisse@gmail.com>
2017-02-28 18:47:16 -0800

    added single argument use case

--- SCALE15x/wireless/configupload.sh
+++ SCALE15x/wireless/configupload.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 cd ./configap
-for i in $(ls -d */)
-do 
-#echo ${i%%/} 
+
+if [ $# -eq 0 ]; then
+  var=`ls -d */`
+else
+  var=$1
+fi
+
+for i in $var
+do
+#echo ${i%%/}
 scp -i ../id_rsa -r ./"${i%%/}"/* root@"${i%%/}":/etc/config/
 ssh -i ../id_rsa root@"${i%%/}" "uci commit"
 done
